c844fd84f4f2ffb2f7065043dc39d1b8bb94b778
malec <alec@ahlbrandt.net>
2019-03-27 10:46:27 -0500

    pre fix

--- pa6_materials/GraphSearch.java
+++ pa6_materials/GraphSearch.java
@@ -1,3 +1,23 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.hadoop.examples;
+
 import java.io.IOException;
 import java.util.Iterator;
 import java.util.List;
@@ -51,14 +71,11 @@ public class GraphSearch extends Configured implements Tool {
 
       // For each GRAY node, emit each of the edges as a new node (also GRAY)
       if (node.getColor() == Node.Color.GRAY) {
-        int index = 0;
-        List<Integer> weights = node.getWeights();
         for (int v : node.getEdges()) {
           Node vnode = new Node(v);
-          vnode.setDistance(node.getDistance() + weights.get(index));
+          vnode.setDistance(node.getDistance() + 1);
           vnode.setColor(Node.Color.GRAY);
           output.collect(new IntWritable(vnode.getId()), vnode.getLine());
-          index++;
         }
         // We're done with this node now, color it BLACK
         node.setColor(Node.Color.BLACK);
--- pa6_materials/Node.java
+++ pa6_materials/Node.java
@@ -1,3 +1,5 @@
+package org.apache.hadoop.examples;
+
 import java.util.*;
 
 import org.apache.commons.logging.Log;
@@ -13,11 +15,10 @@ public class Node {
   private final int id;
   private int distance;
   private List<Integer> edges = new ArrayList<Integer>();
-  private List<Integer> weights = new ArrayList<Integer>();
   private Color color = Color.WHITE;
 
   public Node(String str) {
-    System.out.println(str);
+
     String[] map = str.split("\t");
     String key = map[0];
     String value = map[1];
@@ -31,24 +32,14 @@ public class Node {
         edges.add(Integer.parseInt(s));
       }
     }
-
-    for (String weight : tokens[1].split(",")) {
-      if(weight.length() > 0) {
-        weights.add(Integer.parseInt(weight));
-      }
-    }
     
-    for(int i = 0; i < tokens.length; i++) {
-      System.out.println("Token " + i + ": " + tokens[i]);
-    }
-
-    if (tokens[2].equals("Integer.MAX_VALUE")) {
+    if (tokens[1].equals("Integer.MAX_VALUE")) {
       this.distance = Integer.MAX_VALUE;
     } else {
-      this.distance = Integer.parseInt(tokens[2]);
+      this.distance = Integer.parseInt(tokens[1]);
     }
-    this.color = Color.valueOf(tokens[3]);
     
+    this.color = Color.valueOf(tokens[2]);
 
   }
 
@@ -62,10 +53,6 @@ public class Node {
 
   public int getDistance() {
     return this.distance;
-  }
-
-  public List<Integer> getWeights() {
-    return this.weights;
   }
 
   public void setDistance(int distance) {
@@ -93,11 +80,6 @@ public class Node {
     
     for (int v : edges) {
       s.append(v).append(",");
-    }
-    s.append("|");
-
-    for (int weight: weights) {
-      s.append(weight).append(",");
     }
     s.append("|");
 
